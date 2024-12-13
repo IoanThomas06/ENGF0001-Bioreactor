55b5d5b38aae7cc71933989ca6a5f6f01a6cce6f
Ioan <130181372+IoanThomas06@users.noreply.github.com>
2024-12-13 21:20:50 +0000

    Update wifi_connection.h

--- integrated_with_subsystems/bioreactor_esp32/wifi_connection.h
+++ integrated_with_subsystems/bioreactor_esp32/wifi_connection.h
@@ -8,8 +8,8 @@
 // For example: https://support.microsoft.com/en-us/windows/use-your-windows-pc-as-a-mobile-hotspot-c89b0fad-72d5-41e8-f7ea-406ad9036b85
 // Using a Windows hotspot to creat a LAN is how this has been used for the demonstration:
 // Settings > Network & Internet > Mobile Hotspot > Toggle On/Off
-const char *ssid = "FUCK 4738";  
-const char *password = "$8S87o06";
+const char *ssid = "SSID";  
+const char *password = "PASSWORD";
 
 void connect_to_wifi()
 {
